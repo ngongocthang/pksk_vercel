a4871c09a438dea6ca795d436b1d5e5ad2157a78
hnmtri204 <hoangngocminhtri.thpt.ht@gmail.com>
2024-11-20 00:25:09 +0700

    upload

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,67 +7,62 @@ const Navbar = () => {
   const navigate = useNavigate();
   const { user, setUser } = useContext(AppContext);
   const [showMenu, setShowMenu] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [hasNewNotifications, setHasNewNotifications] = useState(false);
+  const [notificationsCount, setNotificationsCount] = useState(0);
+  const [rotateIcon, setRotateIcon] = useState(false);
+
+  const getDisplayName = (fullName) => {
+    const nameParts = fullName.split(" ");
+    return nameParts.slice(-2).join(" ");
+  };
 
   useEffect(() => {
     const savedUser = localStorage.getItem("user");
     if (savedUser) {
-      const user = JSON.parse(savedUser);
-      setUser(user);
-    } else {
-      console.log("No user data found in localStorage");
+      setUser(JSON.parse(savedUser));
     }
     setLoading(false);
   }, [setUser]);
 
+  useEffect(() => {
+    if (user) {
+      const fetchNotifications = async () => {
+        try {
+          const response = await fetch("http://localhost:5000/notification/find-all", {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          });
+          if (response.ok) {
+            const notifications = await response.json();
+            const unreadCount = notifications.filter((notification) => !notification.read).length;
+            setNotificationsCount(unreadCount);
+          } else {
+            console.error("Không tìm nạp được thông báo");
+          }
+        } catch (error) {
+          console.error("Lỗi tìm nạp thông báo:", error);
+        }
+      };
+      fetchNotifications();
+    }
+  }, [user]);
+
   const handleLogout = () => {
     localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    localStorage.removeItem("hasNewNotifications");
-    localStorage.removeItem("medicalRecords");
     setUser(null);
     navigate("/account");
   };
 
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      if (!user) return;
-
-      try {
-        const response = await fetch("http://localhost:5000/notification", {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        if (!response.ok) throw new Error("Lỗi khi lấy thông báo");
-        const notifications = await response.json();
-        const unreadNotifications = notifications.some(
-          (notification) => !notification.read
-        );
-        setHasNewNotifications(unreadNotifications);
-
-        localStorage.setItem("hasNewNotifications", unreadNotifications.toString());
-      } catch (error) {
-        console.error("Lỗi khi lấy thông báo:", error);
-      }
-    };
-
-  }, []);
-
-  useEffect(() => {
-    const savedNotificationState = localStorage.getItem("hasNewNotifications");
-    if (savedNotificationState === "true") {
-      setHasNewNotifications(true);
-    } else if (savedNotificationState === "false") {
-      setHasNewNotifications(false);
-    }
-  }, []);  
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setRotateIcon(!rotateIcon);
+  };
 
   const handleNotificationClick = () => {
-    setHasNewNotifications(false);
-    localStorage.setItem("hasNewNotifications", "false"); 
-    navigate("/Notifications"); 
+    setNotificationsCount(0);
+    navigate("/Notifications");
   };
 
   if (loading) {
@@ -87,7 +82,7 @@ const Navbar = () => {
         alt="Logo"
         style={{ width: "80px", height: "80px", objectFit: "contain" }}
       />
-  
+
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to="/">
           <li className="py-1 text-base">Trang chủ</li>
@@ -101,57 +96,71 @@ const Navbar = () => {
         <NavLink to="/contact">
           <li className="py-1 text-base">Liên hệ</li>
         </NavLink>
+        <NavLink to="/all-schedule">
+          <button className="bg-[#00759c] text-white px-6 py-2 rounded-full font-light hidden md:block">
+            Đặt lịch hẹn
+          </button>
+        </NavLink>
       </ul>
-  
       <div className="flex items-center gap-4">
         {user ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
+          <div className="flex items-center gap-2 cursor-pointer group relative" onClick={() => setShowDropdown(!showDropdown)}>
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="Avatar" />
-            <p className="font-medium text-gray-700">{user.name}</p>
+            <p className="font-medium text-gray-700">{getDisplayName(user.name)}</p>
             <img className="w-2.5" src={assets.dropdown_icon} alt="Dropdown Icon" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
-              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
-                <p onClick={() => navigate('my-profile')} className="hover:text-black cursor-pointer">Hồ sơ của tôi</p>
-                <p onClick={() => navigate('my-appointments')} className="hover:text-black cursor-pointer">Lịch hẹn của tôi</p>
-                <p onClick={() => navigate('medical-history')} className="hover:text-black cursor-pointer">Lịch sử khám bệnh</p>
-                <p onClick={handleLogout} className="hover:text-black cursor-pointer">Đăng xuất</p>
+            {showDropdown && (
+              <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20">
+                <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                  <p onClick={() => navigate('my-profile')} className="hover:text-black cursor-pointer">Hồ sơ của tôi</p>
+                  <p onClick={() => navigate('my-appointments')} className="hover:text-black cursor-pointer">Lịch hẹn của tôi</p>
+                  <p onClick={() => navigate('medical-history')} className="hover:text-black cursor-pointer">Lịch sử khám bệnh</p>
+                  <p onClick={handleLogout} className="hover:text-black cursor-pointer">Đăng xuất</p>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         ) : (
           <NavLink to="/account">
-            <button onClick={() => navigate('/account')} className="bg-[#00759c] text-white px-8 py-3 rounded-full font-light hidden md:block">
+            <button className="bg-[#00759c] text-white px-8 py-3 rounded-full font-light hidden md:block">
               Đăng nhập
             </button>
           </NavLink>
         )}
-  
-        {/* Hiển thị chuông thông báo chỉ khi người dùng đã đăng nhập */}
+
         {user && (
-          <div className="relative">
-            {hasNewNotifications && (
-              <span className="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 rounded-full"></span>
-            )}
-            <img
-              onClick={handleNotificationClick}
-              className="w-6 cursor-pointer"
-              src={assets.notification_icon}
-              alt="Thông báo"
-            />
-          </div>
+          <>
+            <div className="relative">
+              <img
+                onClick={handleNotificationClick}
+                className="w-6 cursor-pointer"
+                src={assets.notification_icon}
+                alt="Thông báo"
+              />
+              {notificationsCount > 0 && (
+                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
+                  {notificationsCount}
+                </span>
+              )}
+            </div>
+          </>
         )}
-  
+
         <img
-          onClick={() => setShowMenu(true)}
+          onClick={toggleMenu}
           className="w-6 md:hidden"
           src={assets.menu_icon}
           alt="Menu"
         />
-        {/* Mobile Menu */}
-        <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+
+        <div className={`transition-all duration-500 ${showMenu ? "fixed w-full top-0 right-0 bottom-0 z-20 bg-white" : "h-0 w-0 overflow-hidden"}`}>
           <div className="flex items-center justify-between px-5 py-6">
             <img className="w-20" src={assets.logo} alt="Logo" />
-            <img className="w-7" onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="Close" />
+            <img
+              className={`w-7 transform transition-transform duration-300 ease-in-out ${rotateIcon ? "rotate-180" : ""}`}
+              onClick={toggleMenu}
+              src={assets.cross_icon}
+              alt="Close"
+            />
           </div>
           <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
             <NavLink onClick={() => setShowMenu(false)} to="/">
@@ -166,7 +175,7 @@ const Navbar = () => {
             <NavLink onClick={() => setShowMenu(false)} to="/contact">
               <p className="px-4 py-2 rounded inline-block">Liên hệ</p>
             </NavLink>
-            {user ? null : (
+            {!user && (
               <NavLink onClick={() => setShowMenu(false)} to="/account">
                 <p className="bg-[#00759c] text-white px-4 py-2 rounded font-light text-center w-full">Đăng nhập</p>
               </NavLink>
