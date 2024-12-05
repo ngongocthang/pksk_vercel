3d3d7a0ceb57d2e0aa7d855350eb7218291e9ba3
ngongocthang <ngongocthang18082004@gmail.com>
2024-12-05 22:46:58 +0700

    any thing style night 5/12

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -87,8 +87,11 @@ const MyAppointments = () => {
 
   const handleCancelAppointment = (appointmentId) => {
     const appointment = appointments.find((appt) => appt._id === appointmentId);
-    const appointmentDate = new Date(appointment.work_date).toLocaleDateString("vi-VN");
-    const appointmentShift = appointment.work_shift === "morning" ? "Buổi sáng" : "Buổi chiều";
+    const appointmentDate = new Date(appointment.work_date).toLocaleDateString(
+      "vi-VN"
+    );
+    const appointmentShift =
+      appointment.work_shift === "morning" ? "Buổi sáng" : "Buổi chiều";
 
     const confirmCancel = async () => {
       const token = localStorage.getItem("token");
@@ -111,14 +114,16 @@ const MyAppointments = () => {
         );
 
         if (response.data.success) {
+          toast.error("Có lỗi xảy ra khi hủy cuộc hẹn.");
+        } else {
           toast.success("Cuộc hẹn đã được hủy thành công!");
           fetchAppointments();
-        } else {
-          toast.error("Có lỗi xảy ra khi hủy cuộc hẹn.");
         }
       } catch (error) {
         console.error("Error canceling appointment:", error);
-        toast.error("Bạn chỉ huỷ cuộc hẹn trước 24h." || error.response?.data?.message);
+        toast.error(
+          "Bạn chỉ huỷ cuộc hẹn trước 24h." || error.response?.data?.message
+        );
       } finally {
         setIsLoadingCancel(null);
       }
@@ -128,11 +133,11 @@ const MyAppointments = () => {
       toastId.current = toast.warn(
         <div className="flex flex-col items-center justify-center">
           <div className="flex items-center mb-2">
-            <i className="fas fa-info-circle text-blue-500 text-2xl mr-2"></i>
             <p className="font-bold text-lg">Thông báo</p>
           </div>
           <p>
-            Bạn có chắc chắn muốn hủy cuộc hẹn ngày {appointmentDate} vào {appointmentShift} này không?
+            Bạn có chắc chắn muốn hủy cuộc hẹn ngày {appointmentDate} vào{" "}
+            {appointmentShift} này không?
           </p>
           <div className="flex justify-center gap-4 mt-4">
             <button
@@ -206,89 +211,155 @@ const MyAppointments = () => {
         {loading ? (
           <p className="text-center text-gray-500 mt-5">Đang tải dữ liệu...</p>
         ) : appointments.length === 0 ? (
-          <p className="text-center text-gray-500 mt-5">Hiện tại bạn không có lịch hẹn.</p>
+          <p className="text-center text-gray-500 mt-5">
+            Hiện tại bạn không có lịch hẹn.
+          </p>
         ) : (
-          appointments.filter(appointment => showCanceled || appointment.status !== "canceled").map((appointment) => (
-            <div
-              className="relative grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
-              key={appointment._id}
-            >
-              <div>
-                <img
-                  className="w-32 bg-indigo-50"
-                  src={
-                    appointment.doctor_id
-                      ? appointment.doctor_id.user_id.image
-                      : "Không có ảnh"
-                  }
-                  alt="Doctor"
-                />
+          appointments
+            .filter(
+              (appointment) => showCanceled || appointment.status !== "canceled"
+            )
+            .map((appointment) => (
+              <div
+                className="relative grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
+                key={appointment._id}
+              >
+                <div>
+                  <img
+                    className="w-32 bg-indigo-50"
+                    src={
+                      appointment.doctor_id
+                        ? appointment.doctor_id.user_id.image
+                        : "Không có ảnh"
+                    }
+                    alt="Doctor"
+                  />
+                </div>
+                <div className="flex-1 text-sm text-zinc-600">
+                  <p className="text-lg text-neutral-800 font-semibold">
+                    Bệnh nhân:{" "}
+                    {appointment.patient_id
+                      ? appointment.patient_id.user_id.name
+                      : "Không có tên"}
+                  </p>
+                  <p className="text-neutral-800 font-semibold">
+                    Bác sĩ:{" "}
+                    {appointment.doctor_id
+                      ? appointment.doctor_id.user_id.name
+                      : "Không có tên"}
+                  </p>
+                  <p className="text-xs mt-1">
+                    <span className="text-sm text-neutral-700 font-medium">
+                      Ngày khám:
+                    </span>{" "}
+                    {new Date(appointment.work_date).toLocaleDateString(
+                      "vi-VN"
+                    )}
+                  </p>
+                  <p className="text-xs mt-1">
+                    <span className="text-sm text-neutral-700 font-medium">
+                      Ca khám:
+                    </span>{" "}
+                    {appointment.work_shift === "morning"
+                      ? "Buổi sáng"
+                      : "Buổi chiều"}
+                  </p>
+                  <p className="text-xs mt-1">
+                    <span className="text-sm text-neutral-700 font-medium">
+                      Trạng thái:
+                    </span>{" "}
+                    <span
+                      className={`${
+                        appointment.status === "pending"
+                          ? "text-yellow-500"
+                          : appointment.status === "confirmed"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {appointment.status === "pending"
+                        ? "Đang chờ"
+                        : appointment.status === "confirmed"
+                        ? "Đã xác nhận"
+                        : "Đã hủy"}
+                    </span>
+                  </p>
+                  <p className="text-xs mt-1">
+                    <span className="text-sm text-neutral-700 font-medium">
+                      Giá khám:
+                    </span>{" "}
+                    {appointment.doctor_id
+                      ? formatPrice(appointment.doctor_id.price)
+                      : "0"}{" "}
+                    (VND)
+                  </p>
+                  <p className="text-xs mt-1">
+                    <span className="text-sm text-neutral-700 font-medium">
+                      Trạng thái thanh toán:
+                    </span>{" "}
+                    <span
+                      className={`${
+                        appointment.paymentStatus
+                          ? "text-green-500"
+                          : "text-yellow-500"
+                      }`}
+                    >
+                      {appointment.paymentStatus
+                        ? "Đã thanh toán"
+                        : "Chưa thanh toán"}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2 justify-end">
+                  {/* Nút thanh toán */}
+                  <button
+                    onClick={() =>
+                      handlePayment(
+                        appointment._id,
+                        appointment.doctor_id.price
+                      )
+                    }
+                    className={`text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded transition-all duration-300 ${
+                      !appointment.paymentStatus &&
+                      appointment.status === "confirmed"
+                        ? "hover:bg-primary hover:text-white"
+                        : "bg-gray-300 cursor-not-allowed"
+                    }`}
+                    disabled={
+                      appointment.status !== "confirmed" ||
+                      appointment.paymentStatus
+                    }
+                  >
+                    Thanh toán trực tuyến
+                  </button>
+                  {/* Nút hủy */}
+                  <button
+                    onClick={() => handleCancelAppointment(appointment._id)}
+                    className={`text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded transition-all duration-300 ${
+                      appointment.paymentStatus ||
+                      appointment.status === "canceled"
+                        ? "bg-gray-300 cursor-not-allowed"
+                        : "hover:bg-red-600 hover:text-white"
+                    }`}
+                    disabled={
+                      appointment.paymentStatus ||
+                      appointment.status === "canceled" ||
+                      isLoadingCancel === appointment._id
+                    }
+                  >
+                    {isLoadingCancel === appointment._id
+                      ? "Đang hủy..."
+                      : "Hủy cuộc hẹn"}
+                  </button>
+                </div>
               </div>
-              <div className="flex-1 text-sm text-zinc-600">
-                <p className="text-lg text-neutral-800 font-semibold">
-                  Bệnh nhân:{" "}
-                  {appointment.patient_id
-                    ? appointment.patient_id.user_id.name
-                    : "Không có tên"}
-                </p>
-                <p className="text-neutral-800 font-semibold">
-                  Bác sĩ:{" "}
-                  {appointment.doctor_id
-                    ? appointment.doctor_id.user_id.name
-                    : "Không có tên"}
-                </p>
-                <p className="text-xs mt-1">
-                  <span className="text-sm text-neutral-700 font-medium">Ngày khám:</span>{" "}
-                  {new Date(appointment.work_date).toLocaleDateString("vi-VN")}
-                </p>
-                <p className="text-xs mt-1">
-                  <span className="text-sm text-neutral-700 font-medium">Ca khám:</span>{" "}
-                  {appointment.work_shift === "morning" ? "Buổi sáng" : "Buổi chiều"}
-                </p>
-                <p className="text-xs mt-1">
-                  <span className="text-sm text-neutral-700 font-medium">Trạng thái:</span>{" "}
-                  <span className={`${appointment.status === "pending" ? "text-yellow-500" : appointment.status === "confirmed" ? "text-green-500" : "text-red-500"}`}>
-                    {appointment.status === "pending"
-                      ? "Đang chờ"
-                      : appointment.status === "confirmed"
-                      ? "Đã xác nhận"
-                      : "Đã hủy"}
-                  </span>
-                </p>
-                <p className="text-xs mt-1">
-                  <span className="text-sm text-neutral-700 font-medium">Giá khám:</span>{" "}
-                  {appointment.doctor_id ? formatPrice(appointment.doctor_id.price) : "0"} (VND)
-                </p>
-                <p className="text-xs mt-1">
-                  <span className="text-sm text-neutral-700 font-medium">Trạng thái thanh toán:</span>{" "}
-                  <span className={`${appointment.paymentStatus ? "text-green-500" : "text-yellow-500"}`}>
-                    {appointment.paymentStatus ? "Đã thanh toán" : "Chưa thanh toán"}
-                  </span>
-                </p>
-              </div>
-              <div className="flex flex-col gap-2 justify-end">
-                {/* Nút thanh toán */}
-                <button
-                  onClick={() => handlePayment(appointment._id, appointment.doctor_id.price)}
-                  className={`text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded transition-all duration-300 ${!appointment.paymentStatus && appointment.status === "confirmed" ? "hover:bg-primary hover:text-white" : "bg-gray-300 cursor-not-allowed"}`}
-                  disabled={appointment.status !== "confirmed" || appointment.paymentStatus}
-                >
-                  Thanh toán trực tuyến
-                </button>
-                {/* Nút hủy */}
-                <button
-                  onClick={() => handleCancelAppointment(appointment._id)}
-                  className={`text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded transition-all duration-300 ${appointment.paymentStatus || appointment.status === "canceled" ? "bg-gray-300 cursor-not-allowed" : "hover:bg-red-600 hover:text-white"}`}
-                  disabled={appointment.paymentStatus || appointment.status === "canceled" || isLoadingCancel === appointment._id}
-                >
-                  {isLoadingCancel === appointment._id ? "Đang hủy..." : "Hủy cuộc hẹn"}
-                </button>
-              </div>
-            </div>
-          ))
+            ))
         )}
         {error && (
-          <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
+          <div
+            className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg"
+            role="alert"
+          >
             <span className="font-medium">Lỗi:</span> {error}
           </div>
         )}
