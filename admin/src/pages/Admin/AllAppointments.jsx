aad3b8fe4165db335c7c74d7f5ad39f0c7c9e911
hnmtri204 <hoangngocminhtri.thpt.ht@gmail.com>
2024-12-06 14:36:02 +0700

    fix merge 14:40

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -72,11 +72,11 @@ const AllAppointments = () => {
                 {formatDate(item.work_date)}
               </div>
 
-              <div className="flex items-center mb-2 md:mb-0 justify-start md:justify-center gap-2 -mt-0.5">
+              <div className="flex items-center mb-2 md:mb-0 justify-start md:justify-center gap-2">
                 <span className="sm:hidden font-semibold">Ca: </span>
                 <span
                   className={`py-1 px-2 rounded-full text-white text-sm text-center font-semibold
-                    ${item.work_shift === "afternoon" ? "bg-orange-300" : "bg-blue-400"} shadow-lg max-w-[100px] w-full h-[28px]`}
+                    ${item.work_shift === "afternoon" ? "bg-orange-300" : "bg-blue-400"} shadow-lg max-w-[100px] w-full h-[30px]`}
                 >
                   {item.work_shift === "morning" ? "Sáng" : "Chiều"}
                 </span>
